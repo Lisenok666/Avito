716d5ead7c861b3ca1a313d74a839cfecd67f9b7
Lisenok666 <57603720+Lisenok666@users.noreply.github.com>
2022-12-31 17:33:44 +0300

    code after code formatter

--- python 5/cli.py
+++ python 5/cli.py
@@ -4,9 +4,7 @@ from random import randint
 
 
 def log(mask):
-
     def decorator(func):
-
         def wrapper(pizza):
 
             print(mask.format(func(pizza)))
@@ -17,20 +15,20 @@ def log(mask):
     return decorator
 
 
-@log('🍳‍ Приготовили за {}с!')
+@log("🍳‍ Приготовили за {}с!")
 def bake(pizza):
     """Готовит пиццу"""
     cook_time = pizza.cook_time + randint(1, 5)
-    if pizza.size == 'XL':
+    if pizza.size == "XL":
         cook_time *= 2
     return cook_time
 
 
-@log('🏍 Доставили за {}с!')
+@log("🏍 Доставили за {}с!")
 def delivery(pizza):
     """Доставляет пиццу"""
     is_big = 0
-    if pizza.size == 'XL':
+    if pizza.size == "XL":
         is_big = 1
     sndart_time = randint(1, 10) + is_big * randint(1, 5)
     return sndart_time
@@ -40,7 +38,7 @@ def delivery(pizza):
 delivery_alt = delivery
 
 
-@log('🏡 Забрали за {}с!')
+@log("🏡 Забрали за {}с!")
 def pickup(pizza):
     """Самовывоз"""
     pass
@@ -51,17 +49,21 @@ def pickup(pizza):
 def cli(ctx):
     ctx.ensure_object(dict)
     # MENU_PIZZA = {'pepperoni': Pepperoni(), 'margherita': Margherita(), 'hawaiian': Hawaiian()}
-    ctx.obj['MENU_PIZZA'] = {'pepperoni': Pepperoni(), 'margherita': Margherita(), 'hawaiian': Hawaiian()}
+    ctx.obj["MENU_PIZZA"] = {
+        "pepperoni": Pepperoni(),
+        "margherita": Margherita(),
+        "hawaiian": Hawaiian(),
+    }
 
 
 @cli.command()
-@click.option('--delivery', default=False, is_flag=True)
-@click.option('--big_size', default=False, is_flag=True)
-@click.argument('pizza', nargs=1)
+@click.option("--delivery", default=False, is_flag=True)
+@click.option("--big_size", default=False, is_flag=True)
+@click.argument("pizza", nargs=1)
 @click.pass_context
 def order(ctx, pizza: str, big_size: bool, delivery: bool):
     """Готовит и доставляет пиццу"""
-    pizza = ctx.obj['MENU_PIZZA'][pizza]
+    pizza = ctx.obj["MENU_PIZZA"][pizza]
     if big_size:
         pizza.make_big()
 
@@ -76,59 +78,56 @@ def order(ctx, pizza: str, big_size: bool, delivery: bool):
 @click.pass_context
 def menu(ctx):
     """Выводит меню"""
-    for pizza in ctx.obj['MENU_PIZZA'].values():
+    for pizza in ctx.obj["MENU_PIZZA"].values():
         click.echo(pizza.dict())
 
 
 class PizzaRecipe:
-    picture = ' 😉: '
+    picture = " 😉: "
 
     def __init__(self, name, size, recipe, cook_time=1):
-        if size not in ['L', 'XL']:
-            raise ValueError('Size must be L or XL')
+        if size not in ["L", "XL"]:
+            raise ValueError("Size must be L or XL")
         self.name = name
         self.size = size
         self.cook_time = cook_time
         self.recipe = recipe
 
     def make_big(self):
-        self.size = 'XL'
+        self.size = "XL"
 
     def dict(self):
-        ret = ' - ' + str(self.name) + self.picture
-        ret += ', '.join(self.recipe)
+        ret = " - " + str(self.name) + self.picture
+        ret += ", ".join(self.recipe)
         return ret
 
 
 class Margherita(PizzaRecipe):
-    picture = ' 🧀: '
+    picture = " 🧀: "
 
-
-    def __init__(self, size='L'):
-        recipe = ['tomato sauce', 'mozzarella', 'tomatoes']
+    def __init__(self, size="L"):
+        recipe = ["tomato sauce", "mozzarella", "tomatoes"]
         cook_time = 3
-        super(Margherita, self).__init__('Margherita', size, recipe, cook_time)
+        super(Margherita, self).__init__("Margherita", size, recipe, cook_time)
 
 
 class Pepperoni(PizzaRecipe):
-    picture = ' 🍕: '
+    picture = " 🍕: "
 
-
-    def __init__(self, size='L'):
-        recipe = ['tomato sauce', 'mozzarella', 'pepperoni']
+    def __init__(self, size="L"):
+        recipe = ["tomato sauce", "mozzarella", "pepperoni"]
         cook_time = 5
-        super(Pepperoni, self).__init__('Pepperoni', size, recipe, cook_time)
+        super(Pepperoni, self).__init__("Pepperoni", size, recipe, cook_time)
 
 
 class Hawaiian(PizzaRecipe):
-    picture = ' 🍍: '
+    picture = " 🍍: "
 
-
-    def __init__(self, size='L'):
-        recipe = ['tomato sauce', 'mozzarella', 'chicken', 'pineapples']
+    def __init__(self, size="L"):
+        recipe = ["tomato sauce", "mozzarella", "chicken", "pineapples"]
         cook_time = 7
-        super(Hawaiian, self).__init__('Hawaiian', size, recipe, cook_time)
+        super(Hawaiian, self).__init__("Hawaiian", size, recipe, cook_time)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli(obj={})
